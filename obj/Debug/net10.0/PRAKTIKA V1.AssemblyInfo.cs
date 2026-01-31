d40df61287318b9034290316124adccd3154e893
quinta8848 <lolbidenko@gmail.com>
2026-01-31 14:12:08 +0300

    Backend: добавлен сервис для работы SQLite

--- obj/Debug/net10.0/PRAKTIKA V1.AssemblyInfo.cs
+++ obj/Debug/net10.0/PRAKTIKA V1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRAKTIKA_V1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6c5396062a5e02887aa01239a4e4f95c47453f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d761d171cf1b43a19e1e98be551c8563200b1c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRAKTIKA_V1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRAKTIKA_V1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
